3669a644f00a8b835034cd4ecc5fbf762aaa6903
Andrea Iannì <andreaianni@hotmail.com>
2025-02-18 15:58:33 +0100

    Impostata la vista di default

--- RegistroInterrogazioni/App.axaml.cs
+++ RegistroInterrogazioni/App.axaml.cs
@@ -30,9 +30,7 @@ public partial class App : Application
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
-            singleViewPlatform.MainView = new EditCourseView
-            {
-            };
+            singleViewPlatform.MainView = new NavigationView();
         }
 
         base.OnFrameworkInitializationCompleted();
