3a9f5259a188faa5149aaea77a6d6648ae16073f
Andrea Iannì <andreaianni@hotmail.com>
2025-02-18 15:51:58 +0100

    Aggiunte delle funzionalità per la navigazione nel viewmodel di base

--- RegistroInterrogazioni/ViewModels/ViewModelBase.cs
+++ RegistroInterrogazioni/ViewModels/ViewModelBase.cs
@@ -1,7 +1,61 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Avalonia.Interactivity;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
 
 namespace RegistroInterrogazioni.ViewModels;
 
-public class ViewModelBase : ObservableObject
+public partial class ViewModelBase : ObservableObject
 {
+	[ObservableProperty]
+	private ViewModelBase? _parent = null;
+
+	partial void OnParentChanged(ViewModelBase? oldValue, ViewModelBase? newValue)
+	{
+		if (oldValue != null)
+			oldValue.Children.Remove(this);
+		if (newValue != null && !newValue.Children.Contains(this))
+			newValue.Children.Add(this);
+	}
+
+	protected List<ViewModelBase> Children { get; } = new List<ViewModelBase>();
+
+
+	protected virtual void OnReturnHome(RoutedEventArgs eventArgs)
+	{
+	}
+	protected virtual void OnReturnBack(RoutedEventArgs eventArgs)
+	{
+	}
+
+
+	[RelayCommand()]
+	protected void ReturnHome()
+	{
+		RoutedEventArgs eventArgs = new RoutedEventArgs();
+		foreach (var child in Children)
+		{
+			child.OnReturnHome(eventArgs);
+			if (eventArgs.Handled)
+				return;
+		}
+		OnReturnHome(eventArgs);
+		if (!eventArgs.Handled && Parent != null)
+			Parent.ReturnHome();
+	}
+	[RelayCommand]
+	protected void ReturnBack()
+	{
+		RoutedEventArgs eventArgs = new RoutedEventArgs();
+		foreach (var child in Children)
+		{
+			child.OnReturnBack(eventArgs);
+			if (eventArgs.Handled)
+				return;
+		}
+		OnReturnBack(eventArgs);
+		if (!eventArgs.Handled && Parent != null)
+			Parent.ReturnBack();
+	}
 }
