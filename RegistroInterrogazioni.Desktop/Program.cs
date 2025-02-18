46f5fc6107dca68b963cc3d9c4d0165adc2058e9
Andrea Iannì <andreaianni@hotmail.com>
2025-02-18 09:26:30 +0100

    Spostato il progetto a .net9.0

--- RegistroInterrogazioni.Desktop/Program.cs
+++ RegistroInterrogazioni.Desktop/Program.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-
 using Avalonia;
 
 namespace RegistroInterrogazioni.Desktop;
 
-class Program
+sealed class Program
 {
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
@@ -19,5 +18,4 @@ class Program
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace();
-
 }
--- RegistroInterrogazioni/App.axaml.cs
+++ RegistroInterrogazioni/App.axaml.cs
@@ -30,9 +30,8 @@ public partial class App : Application
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
-            singleViewPlatform.MainView = new MainView
+            singleViewPlatform.MainView = new EditCourseView
             {
-                DataContext = new MainViewModel()
             };
         }
 
