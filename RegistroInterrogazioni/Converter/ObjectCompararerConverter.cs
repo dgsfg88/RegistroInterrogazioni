45a3b133f24bd59b942b8684a171df56a4287c74
Andrea Iannì <andreaianni@hotmail.com>
2025-02-20 10:32:52 +0100

    aggiunto un converter per verificare se due oggetti sono uguali

---
+++ RegistroInterrogazioni/Converter/ObjectCompararerConverter.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia.Data;
+using Avalonia.Data.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroInterrogazioni.Converter
+{
+	internal class ObjectCompararerConverter : IValueConverter
+	{
+		public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+			=> value?.Equals(parameter);
+
+		public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			if (value is bool v && v)
+				return parameter;
+			else
+				return BindingOperations.DoNothing;
+		}
+	}
+}
