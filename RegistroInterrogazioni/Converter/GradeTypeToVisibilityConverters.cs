fe7de4fdc026ae79e1d1848a68a9616fbeca02ce
Andrea Iannì <andreaianni@hotmail.com>
2025-02-20 10:33:41 +0100

    Creati dei converter per gestire la visualizzazione degli input dei voti

---
+++ RegistroInterrogazioni/Converter/GradeTypeToVisibilityConverters.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia.Data.Converters;
+using RegistroInterrogazioni.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroInterrogazioni.Converter
+{
+	internal static class GradeTypeToVisibilityConverters
+	{
+		public static FuncValueConverter<GradeSelected, bool> IsNotNote { get; } =
+			new FuncValueConverter<GradeSelected, bool>(gradeType => gradeType != GradeSelected.Note);
+		public static FuncValueConverter<GradeSelected, bool> IsGradeNote { get; } =
+			new FuncValueConverter<GradeSelected, bool>(gradeType => gradeType == GradeSelected.GradeNote);
+		public static FuncValueConverter<GradeSelected, bool> IsGrade { get; } =
+			new FuncValueConverter<GradeSelected, bool>(gradeType => gradeType == GradeSelected.Grade);
+	}
+}
