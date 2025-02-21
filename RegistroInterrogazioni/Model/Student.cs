4bae5d8572d043ea5b7aa096ff334e1979955f8d
Andrea Iannì <andreaianni@hotmail.com>
2025-02-21 09:56:23 +0100

    aggiunti gli algoritmi per la valutazione degli studenti e l'indicazione di dsa/bes perché servirà in seguito per il sorteggio

--- RegistroInterrogazioni/Model/Student.cs
+++ RegistroInterrogazioni/Model/Student.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,5 +15,52 @@ namespace RegistroInterrogazioni.Model
 		public List<Grade> Grades { get; set; } = new List<Grade>();
 		public List<GradeNote> GradeNotes { get; set; } = new List<GradeNote>();
 		public List<Note> Notes { get; set; } = new List<Note>();
+		public bool IsDSA { get; set; } = false;
+
+		public double GradesWeight 
+		{ 
+			get
+			{
+				var today = DateTime.Now;
+				return Grades.Select(x =>
+				((x.GradeValue < 6 ? x.GradeValue / 2 : x.GradeValue)
+				* x.GradeCost) /
+				Math.Max(0.5, (today - x.DateTime).TotalDays / 7)).Sum();
+			} 
+		}
+
+		public double GradeNotesWeight
+		{
+			get
+			{
+				var today = DateTime.Now;
+				return GradeNotes.Select(x => ((int)x.State) / 2.0 /
+				Math.Max(0.5, (today - x.DateTime).TotalDays / 7)).Sum();
+			}
+		}
+
+		public double PartecipationGrade
+		{
+			get
+			{
+				int p_count = 0;
+				int n_count = 0;
+
+				foreach (var gradeNote in GradeNotes)
+				{
+					switch (gradeNote.State)
+					{
+						case GradeNoteState.Negative:
+							n_count++;
+							break;
+						case GradeNoteState.Positive:
+							p_count++;
+							break;
+					};
+				}
+
+				return Math.Round(((p_count + n_count * -0.6) + n_count) / (p_count + n_count) * 40) / 4;
+			}
+		}
 	}
 }
